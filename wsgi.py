aaaea5bb2b0b5d5a5c64f3335e46285ac6b7a4c0
ollavrova <krocozabr@gmail.com>
2015-02-10 19:03:43 +0200

    fix

--- wsgi.py
+++ wsgi.py
@@ -7,13 +7,16 @@ PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 activate_this = os.path.join(PROJECT_DIR, 'venv', 'bin', 'activate_this.py')
 execfile(activate_this, dict(__file__=activate_this))
-sys.path.append(PROJECT_DIR)
+
+if PROJECT_DIR not in sys.path:
+    sys.path.append(PROJECT_DIR)
 
 app = create_app(config='../local.cfg')
-from flipflop import WSGIServer
+# from flipflop import WSGIServer
 
 
 if __name__ == '__main__':
-    WSGIServer(app).run()
+    # WSGIServer(app).run()
+    app.run()
 
 from library.app import app as application
