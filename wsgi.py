e62cf35e61c35612415aa11c1207bef68beef249
ollavrova <krocozabr@gmail.com>
2015-02-12 00:30:54 +0200

    next fix deploy

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,8 @@ import sys
 PROJECT_DIR = u'/home/krocozabr/mysite/flaskTestTask/'
 
 
-activate_this = os.path.join(PROJECT_DIR, 'venv', 'bin', 'activate_this.py')
-execfile(activate_this, dict(__file__=activate_this))
+# activate_this = os.path.join(PROJECT_DIR, 'venv', 'bin', 'activate_this.py')
+# execfile(activate_this, dict(__file__=activate_this))
 
+activate_this = '/home/krocozabr/.virtualenvs/venv/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
